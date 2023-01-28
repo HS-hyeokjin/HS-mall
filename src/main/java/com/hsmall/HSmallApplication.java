ad0284df6313278b3edf5815103bbbf2daa8fb23
HS-hyeokjin <youngurwls@gmail.com>
2023-01-28 13:46:53 +0900

    Build : Spring Initialization

---
+++ src/main/java/com/hsmall/HSmallApplication.java
@@ -0,0 +1,13 @@
+package com.hsmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HSmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HSmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsmall/HSmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HSmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
